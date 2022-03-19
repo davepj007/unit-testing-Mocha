fad293c9fd485009a2e9133e5c16dffb669c3980
David Pereira <davepj07@gmail.com>
2022-03-19 12:06:15 -0600

    Last tests implemented

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,25 +1,62 @@
-const assert = require('assert');
-const { should } = require('chai')
-const { add } = require('../src/unit-testing');
+const { User } = require('../src/unit-testing.js');
+const axios = require('axios');
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const mock = require('./mock.json');
 
-describe('the add function', () => {
-    it('should add 2 numbers together', () => {
-        const result = add(2,2);
-        assert.equal(result, 4);
+const expect = chai.expect; // Extract expect function from chai library
+chai.use(sinonChai);
+
+describe('the User Class', () => {
+    const sandbox = sinon.createSandbox();
+    let user;
+
+    // Declare the user before starting the testings
+    beforeEach(() => {
+        user = new User('davepj007');
     });
 
-    it('should be able to handle 1 number', () => {
-        const result = add(2);
-        assert.equal(result, 2);
+    // Restorer the sandbox to be clean before the next test.
+    afterEach(() => {
+        sandbox.restore();
     });
 
-    it('should be able to handle 0 number', () => {
-        const result = add();
-        assert.equal(result, 0);
+    it('should get the user id', (done) => {
+        const getStub = sandbox.stub(axios, 'get').resolves({ data: { id : 37641342} });
+
+        user.getUserId() // Call getUserID function from the unit-testing.js file
+            .then(result => {
+                expect(result).to.be.a('number');
+                expect(result).to.be.eq(37641342);
+                expect(getStub).to.have.been.calledOnce; // Restric to be called just once
+                expect(getStub).to.have.been.calledWith('https://api.github.com/users/davepj007');
+                done();
+            })
+            .catch(done);
     });
 
-    it('should return 0 if either argument is not a number', () => {
-        const result = add();
-        assert.equal(result, 0);
+    it('should return a repository if the user can view repos', (done) => {
+        const getStub = sandbox.stub(axios, 'get').resolves({ data: ['repo1', 'repo2', 'repo3']});
+        sandbox.stub(user, 'canViewRepos').value(true);
+        user.getUserRepo(2)
+            .then(response => {
+                expect(response).to.be.eq('repo3');
+                expect(getStub).to.have.been.calledOnceWith('https://api.github.com/users/davepj007/repos');
+                done();
+            })
+            .catch(done);
     });
-})
+
+    it('should return an error if the user cannot view repos', (done) => {
+        const getStub = sandbox.stub(axios, 'get');
+        sandbox.stub(user, 'canViewRepos').value(false);
+
+        user.getUserRepo(2)
+            .catch(error => {
+                expect(error).to.be.eq('Cannot view repos');
+                expect(getStub).to.not.have.been.called;
+                done();
+            });
+    });
+});
